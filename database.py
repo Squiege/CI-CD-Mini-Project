46642c414632ac93c0944e91eebd876a6fca3f3b
Devin Rider <drider539@gmail.com>
2025-01-08 05:10:08 -0800

    Fixing database.py

--- database.py
+++ database.py
@@ -1,8 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 
-class Base(declarative_base):
-    pass
-
-db = SQLAlchemy()
-Base = declarative_base()
+db = SQLAlchemy()  
+Base = declarative_base()  
