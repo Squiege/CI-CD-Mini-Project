6a9c8711f02ae4511513407e20cb0231dd08075c
Devin Rider <drider539@gmail.com>
2025-01-09 16:33:33 -0800

    Adding yaml changes and database connection update

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
 
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'postgresql://ci_cd_mini_project_user:oVppz93WJsVYc17u9ziXjknmH5E6gYok@dpg-ctv6uitds78s738ouko0-a:5432/ci_cd_mini_project'
-    )
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ci_cd_mini_project_user:oVppz93WJsVYc17u9ziXjknmH5E6gYok@dpg-ctv6uitds78s738ouko0-a.oregon-postgres.render.com/ci_cd_mini_project'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
 
     # 'mysql+mysqlconnector://root:%40Deblin312145@localhost/m13_mp' , Old URI
