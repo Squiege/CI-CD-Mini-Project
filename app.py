588d0a152276886c3a99d6173d01c4e0fb59a021
Devin Rider <drider539@gmail.com>
2025-01-11 14:12:51 -0800

    update app.py db

--- app.py
+++ app.py
@@ -141,21 +141,21 @@ def test_database_connection(uri):
 
 app = create_app()
 
-# with app.app_context():
-#     # db.drop_all()
-#     print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-#     db.create_all()
-#     # init_roles_data()
-#     # init_customers_info_data()
-#     # init_roles_customers_data()
+with app.app_context():
+    db.drop_all()
+    print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+    db.create_all()
+    init_roles_data()
+    init_customers_info_data()
+    init_roles_customers_data()
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.drop_all()
-        print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-        db.create_all()
-        # init_roles_data()
-        # init_customers_info_data()
-        # init_roles_customers_data()
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.drop_all()
+#         print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+#         db.create_all()
+#         # init_roles_data()
+#         # init_customers_info_data()
+#         # init_roles_customers_data()
 
-    app.run(debug=True)
+#     app.run(debug=True)
